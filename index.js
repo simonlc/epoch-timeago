2beeafb6c5baed4fcb97121d36ac7da76d7f14e7
Simon Laroche <hi@simon.lc>
2017-02-12 15:16:55 -0500

    Clean up tests file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const times = [
   3.154e+10,
 ];
 
-function constructTimeString(unit, singularString, time, timeIndex) {
+function constructTimeString(unit, singularString, timeIndex, time) {
   // NOTE: we do timeIndex - 1 because the array is offset in timeFunctions.
   if (time >= 2 * times[timeIndex - 1]) {
     return `${Math.floor(time / times[timeIndex - 1])} ${unit}s ago`;
@@ -20,12 +20,12 @@ function constructTimeString(unit, singularString, time, timeIndex) {
 
 const timeFunctions = [
   () => 'just now',
-  constructTimeString.bind(null, 'minute', 'a minute ago'),
-  constructTimeString.bind(null, 'hour', 'an hour ago'),
-  constructTimeString.bind(null, 'day', '1 day ago'),
-  constructTimeString.bind(null, 'week', '1 week ago'),
-  constructTimeString.bind(null, 'month', '1 month ago'),
-  constructTimeString.bind(null, 'year', '1 year ago'),
+  constructTimeString.bind(null, 'minute', 'a minute ago', 1),
+  constructTimeString.bind(null, 'hour', 'an hour ago', 2),
+  constructTimeString.bind(null, 'day', '1 day ago', 3),
+  constructTimeString.bind(null, 'week', '1 week ago', 4),
+  constructTimeString.bind(null, 'month', '1 month ago', 5),
+  constructTimeString.bind(null, 'year', '1 year ago', 6),
 ];
 
 // We include our own binary search because we have more control over how 
@@ -45,7 +45,7 @@ function binarySearchInsert(array, value) {
 function epochAgo(timeStamp) {
   const timeDifference = Date.now() - timeStamp;
   const index = binarySearchInsert(times, timeDifference);
-  const timeAgo = timeFunctions[index](timeDifference, index);
+  const timeAgo = timeFunctions[index](timeDifference);
   return timeAgo;
 }
 
--- test.js
+++ test.js
@@ -2,84 +2,132 @@ import test from 'ava';
 import epochAgo from './index';
 
 // Irregular cases
-test('should return seconds string with negative number', t => {
-  const value = epochAgo(Date.now() + 60000);
-  t.is(value, 'just now');
+test('future date', t => {
+  const actual = epochAgo(Date.now() + 60000);
+  const expected = 'just now';
+
+  t.is(actual, expected,
+    'should return just now');
 });
 
-test('should return seconds string with negative number', t => {
-  const value = epochAgo(String(Date.now() - 6.048e+8 * 2));
-  t.is(value, '2 weeks ago');
+test('date in string format', t => {
+  const actual = epochAgo(String(Date.now() - 6.048e+8 * 2));
+  const expected = '2 weeks ago';
+
+  t.is(actual, expected,
+    'should return expected output');
 });
 
 // Regular cases
-test('should return seconds string', t => {
-  const value = epochAgo(Date.now());
-  t.is(value, 'just now');
+test('right now', t => {
+  const actual = epochAgo(Date.now());
+  const expected = 'just now';
+
+  t.is(actual, expected,
+    'should return just now');
 });
 
-test('should return seconds string', t => {
-  const value = epochAgo(Date.now() - 59999);
-  t.is(value, 'just now');
+test('just under under one minute', t => {
+  const actual = epochAgo(Date.now() - 59999);
+  const expected = 'just now';
+
+  t.is(actual, expected,
+    'should return just now');
 });
 
-test('should return minutes one', t => {
-  const value = epochAgo(Date.now() - 60000);
-  t.is(value, 'a minute ago');
+test('exactly one minute', t => {
+  const actual = epochAgo(Date.now() - 60000);
+  const expected = 'a minute ago';
+
+  t.is(actual, expected,
+    'should return once minutes');
 });
 
-test('should return minutes other', t => {
-  const value = epochAgo(Date.now() - 60000 * 2);
-  t.is(value, '2 minutes ago');
+test('exactly two minutes', t => {
+  const actual = epochAgo(Date.now() - 60000 * 2);
+  const expected = '2 minutes ago';
+
+  t.is(actual, expected,
+    'should return other minutes');
 });
 
 test('should return hours one', t => {
-  const value = epochAgo(Date.now() - 3.6e+6);
-  t.is(value, 'an hour ago');
+  const actual = epochAgo(Date.now() - 3.6e+6);
+  const expected = 'an hour ago';
+
+  t.is(actual, expected,
+    'should return once hours');
 });
 
 test('should return hours other', t => {
-  const value = epochAgo(Date.now() - 3.6e+6 * 2);
-  t.is(value, '2 hours ago');
+  const actual = epochAgo(Date.now() - 3.6e+6 * 2);
+  const expected = '2 hours ago';
+
+  t.is(actual, expected,
+    'should return other hours');
 });
 
 test('should return days one', t => {
-  const value = epochAgo(Date.now() - 8.64e+7);
-  t.is(value, '1 day ago');
+  const actual = epochAgo(Date.now() - 8.64e+7);
+  const expected = '1 day ago';
+
+  t.is(actual, expected,
+    'should return once days');
 });
 
 test('should return days other', t => {
-  const value = epochAgo(Date.now() - 8.64e+7 * 2);
-  t.is(value, '2 days ago');
+  const actual = epochAgo(Date.now() - 8.64e+7 * 2);
+  const expected = '2 days ago';
+
+  t.is(actual, expected,
+    'should return other days');
 });
 
 test('should return weeks one', t => {
-  const value = epochAgo(Date.now() - 6.048e+8);
-  t.is(value, '1 week ago');
+  const actual = epochAgo(Date.now() - 6.048e+8);
+  const expected = '1 week ago';
+
+  t.is(actual, expected,
+    'should return once weeks');
 });
 
 test('should return weeks other', t => {
-  const value = epochAgo(Date.now() - 6.048e+8 * 2);
-  t.is(value, '2 weeks ago');
+  const actual = epochAgo(Date.now() - 6.048e+8 * 2);
+  const expected = '2 weeks ago';
+
+  t.is(actual, expected,
+    'should return other weeks');
 });
 
 test('should return months one', t => {
-  const value = epochAgo(Date.now() - 2.628e+9);
-  t.is(value, '1 month ago');
+  const actual = epochAgo(Date.now() - 2.628e+9);
+  const expected = '1 month ago';
+
+  t.is(actual, expected,
+    'should return once months');
 });
 
 test('should return months other', t => {
-  const value = epochAgo(Date.now() - 2.628e+9 * 2);
-  t.is(value, '2 months ago');
+  const actual = epochAgo(Date.now() - 2.628e+9 * 2);
+  const expected = '2 months ago';
+
+  t.is(actual, expected,
+    'should return other months');
 });
 
 test('should return years one', t => {
-  const value = epochAgo(Date.now() - 3.154e+10);
-  t.is(value, '1 year ago');
+  const actual = epochAgo(Date.now() - 3.154e+10);
+  const expected = '1 year ago';
+
+  t.is(actual, expected,
+    'should return once years');
 });
 
 test('should return years other', t => {
-  const value = epochAgo(Date.now() - 3.154e+10 * 2);
-  t.is(value, '2 years ago');
+  const actual = epochAgo(Date.now() - 3.154e+10 * 2);
+  const expected = '2 years ago';
+
+  t.is(actual, expected,
+    'should return other years');
 });
 
