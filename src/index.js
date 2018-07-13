7380ddae35dab2f8afc407f85aea8214ff04b413
Simon Laroche <hi@simon.lc>
2018-07-13 05:06:45 -0400

    Make output a little smaller

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const timeSegments = [
+let timeSegments = [
   3.154e10,
   2.628e9,
   6.048e8,
@@ -8,24 +8,24 @@ const timeSegments = [
   -Infinity,
 ];
 
-const makeTimeString = (unit, singularString) => (timeSegment, time) =>
+let makeTimeString = (unit, singularString) => (timeSegment, time) =>
   time >= 2 * timeSegment
     ? `${Math.floor(time / timeSegment)} ${unit}s ago`
     : singularString;
 
-const timeFunctions = [
+let timeFunctions = [
   makeTimeString('year', '1 year ago'),
   makeTimeString('month', '1 month ago'),
   makeTimeString('week', '1 week ago'),
   makeTimeString('day', '1 day ago'),
   makeTimeString('hour', 'an hour ago'),
   makeTimeString('minute', 'a minute ago'),
-  () => 'just now',
+  _ => 'just now',
 ];
 
-export default function epochAgo(timeStamp) {
-  const timeDifference = Date.now() - timeStamp;
-  const index = timeSegments.findIndex(time => timeDifference >= time);
-  const timeAgo = timeFunctions[index](timeSegments[index], timeDifference);
+export default timeStamp => {
+  let timeDifference = Date.now() - timeStamp;
+  let index = timeSegments.findIndex(time => timeDifference >= time);
+  let timeAgo = timeFunctions[index](timeSegments[index], timeDifference);
   return timeAgo;
 }
